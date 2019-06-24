82a1b756af1bd7b3767f1e7e3b6696d49861093e
Alexandr Krylov <alexandr.krylov@gmail.com>
2019-06-24 13:23:13 +0600

    ver 4

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,17 @@ struct Rectangle {
     width: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.length * self.width
+    }
+}
+
 fn main() {
     let rect1 = Rectangle { length: 50, width: 30 };
 
-    println!("rect1 is {:#?}", rect1);
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.length * rectangle.width
 }
